c4a34fe45b80f767334affd8db0a1f03757dd0e4
mmdoneperson <mohmaks220@gmail.com>
2025-02-25 17:01:55 +0500

    check winner

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
-const MAP = [
+let MAP = [
     [EMPTY, EMPTY, EMPTY],
     [EMPTY, EMPTY, EMPTY],
     [EMPTY, EMPTY, EMPTY]
 ];
-
+let winner
 let lastMove = ZERO
 
 const container = document.getElementById('fieldWrapper');
@@ -48,6 +48,87 @@ function cellClickHandler(row, col) {
         renderSymbolInCell(ZERO, row, col);
      */
 }
+
+function checkWinner() {
+    for (let i = 0; i < MAP.length; i++) {
+        let temp;
+        for (let j = 0; j < MAP[i].length - 1; j++) {
+            if (MAP[i][j] === MAP[i][j + 1] && MAP[i][j] !== EMPTY)
+                temp = MAP[i][j];
+            else {
+                temp = undefined
+                break;
+            }
+        }
+        if (temp !== undefined) {
+            winner = temp;
+            let res = []
+            for (let j = 0; j < MAP[i].length; j++) {
+                res.push([i, j])
+            }
+            drawCell(res);
+            return;
+        }
+    }
+
+    for (let i = 0; i < MAP.length; i++) {
+        let temp;
+        for (let j = 0; j < MAP[i].length - 1; j++) {
+            if (MAP[j][i] === MAP[j][1 + 1] && MAP[j][i] !== EMPTY)
+                temp = MAP[j][i];
+            else {
+                temp = undefined
+                break;
+            }
+        }
+        if (temp !== undefined) {
+            winner = temp;
+            let res = []
+            for (let j = 0; j < MAP[i].length; j++) {
+                res.push([j, i])
+            }
+            drawCell(res);
+        }
+        return;
+    }
+    let res = []
+    for (let i = 0; i < MAP.length; i++) {
+        if (MAP[i][i] === MAP[i][1 + 1] && MAP[i][i] !== EMPTY) {
+            winner = MAP[i][i];
+            res.push([i, i])
+        } else {
+            winner = undefined;
+            break;
+        }
+    }
+    if (winner !== undefined) {
+        drawCell(res);
+        return;
+    }
+    res = []
+    for (let j = 0; i < MAP.length; i++) {
+        let i = MAP.length - j - 1;
+        if (MAP[i][i] === MAP[i][1 + 1] && MAP[i][i] !== EMPTY) {
+            res.push([i, i])
+            winner = MAP[i][i];
+        } else {
+            winner = undefined;
+            break;
+        }
+    }
+    if (winner !== undefined) {
+        drawCell(res);
+        return;
+    }
+    return;
+}
+
+function drawCell(cells) {
+    for (let i = 0; i < cells.length; i++) {
+        renderSymbolInCell(winner, cells[i][0], cells[i][1], 'red');
+    }
+}
+
 
 function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
